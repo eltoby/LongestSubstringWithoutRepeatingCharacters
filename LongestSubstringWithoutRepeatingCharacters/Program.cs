ca737737f8a30cdc69e999e3913ca0df89fd8448
Pablo Rausch <pablorausch@gmail.com>
2019-06-15 00:02:45 -0300

    Agregar archivos de proyecto.

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            Console.WriteLine(s.LengthOfLongestSubstring("pwwkew"));
+            Console.Read();
+        }
+    }
+}
---
+++ LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var result = 0;
+        var q = new Queue<char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {            
+            if (q.Contains(s[i]))
+            {
+                result = Math.Max(q.Count, result);
+
+                while (q.Dequeue() != s[i]) ;
+
+                if (q.Count + 1 + s.Length - i <= result)
+                    return result;
+            }
+
+            q.Enqueue(s[i]);
+        }
+
+        result = Math.Max(q.Count, result);
+
+        return result;
+    }
+}
